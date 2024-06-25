d26ecd1bd32fcec526a842d8f1853572c4fa23a7
Ubuntu <ubuntu@ip-172-31-62-67.ec2.internal>
2024-06-25 01:23:32 +0000

    Incluindo outros microServices e adicinando o DockerFile para todos

--- hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/devsuperior/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,12 +1,9 @@
 package com.devsuperior.hrapigatewayzuul;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-import java.util.List;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
--- hr-user/src/main/java/com/devsuperior/hruser/resources/UserResouce.java
+++ hr-user/src/main/java/com/devsuperior/hruser/resources/UserResouce.java
@@ -6,7 +6,8 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.util.List;
 
 @RestController
@@ -15,16 +16,37 @@ public class UserResouce {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @GetMapping
+    public ResponseEntity<?> listAll(){
+        List<User> users = userRepository.findAll();
+        
+        return !users.isEmpty() ? ResponseEntity.ok(users) : ResponseEntity.ok().build();
+    }
+	
 
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
         User user = userRepository.findById(id).get();
         return ResponseEntity.ok(user);
     }
-
+    
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String password_enc = encoder.encode(user.getPassword());
+		
+        user.setPassword(password_enc);
+        User createdUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+    
+    
     @GetMapping("/search")
     public ResponseEntity<User> findByEmail(@RequestParam String email){
         User user = userRepository.findByEmail(email);
         return ResponseEntity.ok(user);
     }
+    
 }
